0b07f1b4baf85bc45d9e6101410e3b15315780e6
IASIKO <iaseshviligi@gmail.com>
2023-06-04 17:06:49 +0400

    add

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,19 +1,17 @@
-import { useSelector } from 'react-redux';
-import Card from '../UI/Card';
-import classes from './Cart.module.css';
-import CartItem from './CartItem';
+import { useSelector } from "react-redux";
+import Card from "../UI/Card";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
-  const quantity = useSelector((state) => state.cart.quantity)
-
-
+  const quantity = useSelector((state) => state.cart.quantity);
 
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
         <CartItem
-          item={{ title: 'Test Item', quantity: quantity, total: 18, price: 6 }}
+          item={{ title: "Test Item", quantity: quantity, total: 18, price: 6 }}
         />
       </ul>
     </Card>
