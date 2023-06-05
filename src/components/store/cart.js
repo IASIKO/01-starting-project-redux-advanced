fb14e97851cc470539ea5f85f5e3e5c21cbe88b1
IASIKO <iaseshviligi@gmail.com>
2023-06-05 13:28:20 +0400

    add

--- src/components/store/cart.js
+++ src/components/store/cart.js
@@ -1,6 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initalCartState = { quantity: 0, showCart: false };
+const initalCartState = {
+  items: [],
+  quantity: 0,
+  // showCart: false,
+};
 
 const cartSlice = createSlice({
   name: "cart",
@@ -13,14 +17,20 @@ const cartSlice = createSlice({
       if (state.quantity > 0) {
         state.quantity--;
       }
-      return 
+      return;
     },
-    toggleCart(state) {
-      state.showCart = !state.showCart;
+    // toggleCart(state) {
+    //   state.showCart = !state.showCart;
+    // },
+    addToCart(state, action) {
+      // state.quantity++;
+      const newItem = action.payload
+      const existingItem = state.items.find(item => item.id === newItem.id)
+      if (!existingItem) {
+        state.items.push()
+      }
     },
-    addToCart(state) {
-      state.quantity++
-    }
+    removeFromCart(){}
   },
 });
 
